930edd75ad2bf74b6ccee4922726e19ea643ebca
Hasan <blackbee08@gmail.com>
2021-04-08 20:34:35 +0700

    Added controller

---
+++ src/domain/core/Pokedex.Api/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pokedex.Api.Controllers
+{
+    [ApiVersion("1")]
+    [Route("v{version:apiVersion}/")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("pokemon/{name}")]
+        public IActionResult GetInformation(string name)
+        {
+            return StatusCode(StatusCodes.Status200OK, null);
+        }
+    }
+}
